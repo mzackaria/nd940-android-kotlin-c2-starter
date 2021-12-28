6bc81054117051838f8d179ff78569c5be4c4bd9
Zakaria MESSAI <messai.zackaria@gmail.com>
2021-12-28 21:39:20 +0100

    ...

--- starter/app/src/main/java/com/udacity/asteroidradar/util/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/util/Constants.kt
@@ -4,6 +4,6 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    //I will remove it with all previous commits when project is validated
+    //I will remove it with all previous commits when the project will be validated
     const val API_KEY : String = "IAbOhxz75KIOttVTgFsnKrHTnktM70nuuMu1gxSv"
 }
