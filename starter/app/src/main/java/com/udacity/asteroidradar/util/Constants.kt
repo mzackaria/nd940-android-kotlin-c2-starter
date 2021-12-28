9738c17245183e5931e9aeb95ad362bfdbf752aa
Zakaria MESSAI <messai.zackaria@gmail.com>
2021-12-28 21:27:17 +0100

    1 to 7 days

--- starter/app/src/main/java/com/udacity/asteroidradar/util/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/util/Constants.kt
@@ -2,7 +2,7 @@ package com.udacity.asteroidradar.util
 
 object Constants {
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
-    const val DEFAULT_END_DATE_DAYS = 1
+    const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
     const val API_KEY : String = "IAbOhxz75KIOttVTgFsnKrHTnktM70nuuMu1gxSv"
 }
